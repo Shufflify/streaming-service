9efbb49b153444bdad4e33d1f993392564dc131d
RudyRed <willgross94@gmail.com>
2017-12-19 16:37:51 -0800

    Added headers, fixed and tested stream

--- streaming/server/index.js
+++ streaming/server/index.js
@@ -3,38 +3,23 @@ const fs = require('fs');
 const mp3Duration = require('mp3-duration');
 const aa = require('express-async-await');
 const { queryDB } = require('../database/index.js');
+const { populate } = require('../database/index.js');
+// const AWS = require('aws-sdk');
 
 
 const app = aa(express());
 
-const path = 'server/assets/01 HeLLo (marshmello remix).mp3';
-
-
-mp3Duration(path,  (err, duration) => {
-  if (err) return console.log(err.message);
-  console.log(duration)
-  console.log(`Your file is ${duration / 60} minutes long`);
-  const stat = fs.statSync(path); // Will need to add to DB
-  const fileSize = stat.size;
-  console.log(fileSize) // size in bytes
-  console.log(`The bitrate is ${0.008 * fileSize/duration}`)
-  console.log(`extra: ${fileSize % Math.floor(0.008 * fileSize/duration)}`); // send this extra on second reply
-  console.log(`${0.008 * fileSize/duration * 10} bytes per 10 seconds`)
-});
-// new Buffer(size)
 
 app.get('/:songId/:bytes', async (req, res, next) => {
 // how many bytes are already loaded
   const [songInfo] = await queryDB(`SELECT * FROM songs WHERE id=${req.params.songId}`);
-  const bitrate = Math.floor(8 * (songInfo.filesize / songInfo.duration));
-
+  const bitrate = Math.floor(songInfo.filesize / (songInfo.duration / 1000));
   fs.open(songInfo.filepath, 'r', (status, fd) => {
     if (status) {
       return;
     }
     const startingByte = Number(req.params.bytes);
-    let bufferLength = startingByte !== bitrate * 10 ?
-      bitrate * 10 : (bitrate * 10) + (songInfo.filesize % bitrate);
+    let bufferLength = bitrate * 10;
 
     bufferLength = startingByte + bufferLength < songInfo.filesize ?
       bufferLength : songInfo.filesize - startingByte - 1;
@@ -42,13 +27,27 @@ app.get('/:songId/:bytes', async (req, res, next) => {
     const buffer = new Buffer(bufferLength);
 
     fs.read(fd, buffer, 0, buffer.length, startingByte, (err, num) => {
-      console.log(num)
-      // write headers and such
-      res.send(buffer);
+      const head = {
+        'Content-Range': `bytes ${startingByte}-${startingByte + (buffer.length - 1)}/${songInfo.filesize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': buffer.length,
+        'Content-Type': 'audio/mpeg',
+      };
+      res.writeHead(206, head);
+      res.end(buffer);
     });
   });
 });
 
+queryDB('SELECT count(*) as count FROM songs')
+  .then((results) => {
+    const n = 100000;
+    console.log(results[0].count)
+    console.log(`${n - results[0].count} entires need to be added to the DB`);
+    if (results[0].count < n) {
+      populate(n - results[0].count);
+    }
+  });
 
 const host = '0.0.0.0';
 const port = 8080
