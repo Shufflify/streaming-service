3d72e93157bf665262c1e52aaa31585389421c00
RudyRed <willgross94@gmail.com>
2017-12-22 15:13:32 -0800

    Set up load balancer, set up a route to be used to test cache when the time comes

--- streaming/server/index.js
+++ streaming/server/index.js
@@ -14,12 +14,20 @@ const fs = require('fs');
 const aa = require('express-async-await');
 const { queryDB } = require('../database/index.js');
 const { populate } = require('../database/index.js');
+// const redis = require('redis');
 
 const app = aa(express());
+
+app.get('/x', async (req, res, next) => { // for test cacheing
+  const id = Math.random() > .25 ? Math.floor(Math.random() * 100) : Math.floor(Math.random() * (10000000 - 100)) + 100;
+  const [songInfo] = await queryDB(`SELECT * FROM songs WHERE id=${id}`);
+  res.json(songInfo);
+});
 
 app.get('/:songId/:bytes', async (req, res, next) => {
 // how many bytes are already loaded
   const [songInfo] = await queryDB(`SELECT * FROM songs WHERE id=${req.params.songId}`);
+  // const songInfo = {filepath: `server/assets/01 HUMBLE. (Skrillex Remix).mp3`, duration: 156996, filesize: 6279837}
   const bitrate = Math.floor(songInfo.filesize / (songInfo.duration / 1000));
   fs.open(songInfo.filepath, 'r', (status, fd) => {
     if (status) {
@@ -33,17 +41,24 @@ app.get('/:songId/:bytes', async (req, res, next) => {
 
     const buffer = new Buffer(bufferLength);
 
-    fs.read(fd, buffer, 0, buffer.length, startingByte, (err, num) => {
+    fs.read(fd, buffer, 0, buffer.length, startingByte, (err, bufferSize) => {
+      if (err) {
+        console.log(err, 'THIS IS AN ERROR!!!!');
+        return;
+      }
       const head = {
         'Content-Range': `bytes ${startingByte}-${startingByte + (buffer.length)}/${songInfo.filesize}`,
         'Accept-Ranges': 'bytes',
-        'Content-Length': buffer.length,
+        'Content-Length': bufferSize,
         'Content-Type': 'audio/mpeg',
       };
+      console.log(`${bufferSize} bytes sent`);
       // below is calculation for the starting time of the song chunk IN SECONDS
-      console.log(`${buffer.length} bytes sent`);
       // console.log(Math.floor((startingByte/(songInfo.filesize - (songInfo.filesize % bitrate))) * (songInfo.duration / 1000)));
-      fs.close(fd, () => { console.log('closed'); });
+      fs.close(fd, () => {
+        console.log('closed');
+      });
+      // send of SQS massage to events Microservice here
       res.writeHead(206, head);
       res.end(buffer);
     });
